ea8f99c31c519b365370009b26f83b8e5d632061
Poppe-A <35397614+Poppe-A@users.noreply.github.com>
2021-07-19 17:51:25 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const db = require('./config/db')
+require('dotenv').config()
+const routes = require('./routes/index')
+
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+app.use('/', routes)
+
+const { SERVER_PORT } = process.env
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Connected, listen on port ${SERVER_PORT}`)
+})
